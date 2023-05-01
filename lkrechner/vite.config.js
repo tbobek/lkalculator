0e220b4de8a42917b75c54fa928b278875aa9a1d
tbobek <thomas.bobek@code4tec.de>
2023-05-01 18:55:02 +0200

    vite.config.js

--- lkrechner/vite.config.js
+++ lkrechner/vite.config.js
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/tbobek.github.io',
+  publicDir: '/docs',
   plugins: [vue()],
   resolve: {
     alias: {
